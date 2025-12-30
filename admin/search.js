53b9bdc984f3320ad11856ef768e4a23c9f09a3b
zaycashh <72800461+zaycashh@users.noreply.github.com>
2025-12-30 17:32:16 -0500

    Update search.js

--- admin/search.js
+++ admin/search.js
@@ -159,14 +159,14 @@ window.runSearch = function () {
       endDate.setHours(23, 59, 59, 999);
       break;
 
-    case "custom":
-      if (startInput && endInput) {
-        startDate = new Date(startInput);
-        endDate = new Date(endInput);
-        endDate.setHours(23, 59, 59, 999);
-      }
-      break;
-  }
+    const [sy, sm, sd] = startInput.split("-").map(Number);
+startDate = new Date(sy, sm - 1, sd);
+startDate.setHours(0, 0, 0, 0);
+
+const [ey, em, ed] = endInput.split("-").map(Number);
+endDate = new Date(ey, em - 1, ed);
+endDate.setHours(23, 59, 59, 999);
+
 
   lastSearchStartDate = startDate;
   lastSearchEndDate = endDate;
