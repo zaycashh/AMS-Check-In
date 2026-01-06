811c77c8e05c7e2b899d65b0fb7d1e20365f2fab
zaycashh <72800461+zaycashh@users.noreply.github.com>
2026-01-06 18:56:28 -0500

    Update search.js

--- admin/search.js
+++ admin/search.js
@@ -327,6 +327,8 @@ function exportSearchPdf() {
   });
 }
 
+window.exportSearchPdf = exportSearchPdf;
+
 /* =========================================================
    EXPORT EXCEL
 ========================================================= */
