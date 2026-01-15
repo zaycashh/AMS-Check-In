b049b2191bdbbccf335305f8f33145941be0bf36
zaycashh <72800461+zaycashh@users.noreply.github.com>
2026-01-15 17:51:23 -0500

    Update search.js

--- admin/search.js
+++ admin/search.js
@@ -299,43 +299,43 @@ function renderSearchResults(results) {
     return;
   }
 
-        results.forEach(r => {
-  tbody.innerHTML += `
-    <tr>
-      <td>${r.date}</td>
-      <td>${r.time}</td>
-      <td>${r.first}</td>
-      <td>${r.last}</td>
-      <td>${r.company}</td>
-      <td>${r.reason}</td>
-      <td>${r.services}</td>
-      <td>
-        ${
-          r.signature
-            ? `<img src="${r.signature}" style="width:90px;height:30px;">`
-            : ""
-        }
-      </td>
-      <td style="white-space:nowrap;">
-        ${
-          r.locked !== false
-            ? `<span style="
-                display:inline-block;
-                padding:2px 6px;
-                margin-right:6px;
-                font-size:11px;
-                background:#eee;
-                border-radius:4px;
-                font-weight:600;
-              ">🔒 LOCKED</span>`
-            : ""
-        }
-        <button onclick="deleteDonor('${r.id}')">Delete</button>
-      </td>
-    </tr>
-  `;
-});
-
+  results.forEach(r => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${r.date}</td>
+        <td>${r.time}</td>
+        <td>${r.first}</td>
+        <td>${r.last}</td>
+        <td>${r.company}</td>
+        <td>${r.reason}</td>
+        <td>${r.services}</td>
+        <td>
+          ${
+            r.signature
+              ? `<img src="${r.signature}" style="width:90px;height:30px;">`
+              : ""
+          }
+        </td>
+        <td style="white-space:nowrap;">
+          ${
+            r.locked !== false
+              ? `<span style="
+                  display:inline-block;
+                  padding:2px 6px;
+                  margin-right:6px;
+                  font-size:11px;
+                  background:#eee;
+                  border-radius:4px;
+                  font-weight:600;
+                ">🔒 LOCKED</span>`
+              : ""
+          }
+          <button onclick="deleteDonor('${r.id}')">Delete</button>
+        </td>
+      </tr>
+    `;
+  });
+}
 
 /* =========================================================
    HELPERS
